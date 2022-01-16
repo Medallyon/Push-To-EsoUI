b7c136e344802b7daee6deee2ad714dc68664e6d
Tilman Raendchen <tilman@medallyon.me>
2022-01-16 07:16:58 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,106 +1,72 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const http = require('https');
-const fs = require('fs')
-const FormData = require('form-data');
+const core = require("@actions/core")
+	, github = require("@actions/github")
+	, http = require("https")
+	, fs = require("fs").promises
+	, FormData = require("form-data");
 
-try {
-  const path = core.getInput('path');
-  //const manifestPath = `${path}${core.getInput('addonManifest')}`
-  const readmePath = `${path}${core.getInput('readme')}`
-  const changelogPath = `${path}${core.getInput('changelog')}`
-  const apiToken = core.getInput('EsoUIToken');
-  const id = core.getInput('EsoUIID');
-  const version = core.getInput("version")
-  const artifact = core.getInput("artifact")
+const API_DOMAIN = "https://api.esoui.com";
 
+async function readFile(path, callback)
+{
+	try
+	{
+		callback(await fs.readFile(path, "utf8"));
+	}
 
-  /*
-  let options = {
-    headers: {
-      'x-api-token': apiToken
-    }
-  }
-  get(`https://api.esoui.com/addons/details/${id}.json`, options, (res) => {
-    console.log(res)
-    console.log(res[0].version)
-  })
-  */
-
-
-  readFile(readmePath, (readme) => {
-    outputDescription = replaceMD(readme)
-    readFile(changelogPath, (outputChangelog) => {
-      /*
-      console.log(outputDescription)
-      console.log(outputChangelog)
-      console.log(version)
-      */
-      let data = new FormData();
-      data.append('id', +id);
-      data.append('version', `${version}`);
-      data.append('description', outputDescription)
-      data.append('changelog', outputChangelog)
-      data.append('updatefile', fs.createReadStream(artifact));
-
-
-      var request = http.request('https://api.esoui.com/addons/update', {
-        method: 'post',
-        headers: {'x-api-token': apiToken, ...data.getHeaders()}
-      });
-
-      data.pipe(request);
-
-      request.on('response', function(res) {
-        console.log(res.statusCode);
-      });
-    })
-  })
-  
-
-
-
-
-
-} catch (error) {
-  core.setFailed(error.message);
+	catch (err)
+	{
+		console.error(err);
+	}
 }
 
-
-
-function get(url, options, callback) {
-  http.get(url, options, (res) => {
-    console.log(`STATUS: ${res.statusCode}`);
-    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        callback(parsedData)
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  })
+function replaceMD(text)
+{
+	text = text.replace(/^# (.*?)\n/gm,"[SIZE=5]$1[/SIZE]\n");
+	text = text.replace(/^## (.*?)\n/gm,"[SIZE=4]$1[/SIZE]\n");
+	text = text.replace(/^### (.*?)\n/gm,"[SIZE=3]$1[/SIZE]\n");
+	text = text.replace(/`((?!`).*?)`/gm,"[Highlight]$1[/Highlight]");
+	text = text.replace(/```(.*?)```/gm,"[Highlight=lua]$1[/Highlight]");
+	return text;
 }
 
+try
+{
+	(async function main()
+	{
+		const readmePath = core.getInput("readme")
+			, changelogPath = core.getInput("changelog")
+			, apiToken = core.getInput("EsoUIToken")
+			, id = core.getInput("EsoUIID")
+			, version = core.getInput("version")
+			, artifact = core.getInput("artifact")
+			, dryRun = core.getInput("dryRun");
 
-function readFile(path, callback) {
-  try {
-    callback(fs.readFileSync(path, 'utf8'))
-  } catch (err) {
-    console.error(err)
-  }
+		const data = new FormData();
+		data.append("id", +id);
+		data.append("version", version);
+
+		data.append(replaceMD(await fs.readFile(readmePath, "utf8")));
+		data.append(await fs.readFile(changelogPath, "utf8"));
+		data.append("updatefile", fs.createReadStream(artifact));
+
+		let endpoint = API_DOMAIN + "/addons/update";
+		if (dryRun)
+			endpoint += "test";
+
+		const request = http.request(endpoint, {
+			method: "post",
+			headers: {
+				"x-api-token": apiToken,
+				...data.getHeaders()
+			}
+		});
+
+		data.pipe(request);
+		request.on("response", res => console.log(res.statusCode));
+	})();
 }
 
-
-function replaceMD(text) {
-  text = text.replace(/^# (.*?)\n/gm,'[SIZE=5]$1[/SIZE]\n')
-  text = text.replace(/^## (.*?)\n/gm,'[SIZE=4]$1[/SIZE]\n')
-  text = text.replace(/^### (.*?)\n/gm,'[SIZE=3]$1[/SIZE]\n')
-  text = text.replace(/`((?!`).*?)`/gm,'[Highlight]$1[/Highlight]')
-  text = text.replace(/```(.*?)```/gm,'[Highlight=lua]$1[/Highlight]')
-  return text
+catch (error)
+{
+	core.setFailed(error.message);
 }
